894bb0a75e58e415bf1937e50c967ebc708a7aa6
damienfarrell <damien.farrell1@gmail.com>
2023-04-02 13:13:48 +0100

    Fixed Typing Mistake

--- plottask.py
+++ plottask.py
@@ -1,4 +1,4 @@
-# squareroot.py
+# plottask.py
 # This program  displays a histogram of a normal distribution of a 1000 values with a mean of 5 and standard deviation of 2, 
 # and a plot of the function h(x)=x3 in the range [0, 10], on the one set of axes.
 # Author: Damien Farrell
